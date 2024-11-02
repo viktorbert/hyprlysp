12794c426eb3adf675e5bf16fb6ec1884fd90909
Viktor Bert <viktorbert888@gmail.com>
2024-11-02 12:49:36 +0000

    Set up types

--- implementation/types/types.go
+++ implementation/types/types.go
@@ -1,5 +1,49 @@
 package types
 
+import (
+	"math/big"
+)
+
+
 type HLValue interface{}
 
+type HLBigInt struct {
+	Value *big.Int
+}
 
+type HLSysInt struct {
+	Value int
+}
+
+type HLByte struct {
+	Value uint8
+}
+
+type HLString struct {
+	Value String
+}
+
+type HLSymbol struct {
+	Value String
+}
+
+type HLKeyword struct {
+	Value String
+}
+
+
+type HLLiteral string
+
+const (
+	HLNil   HLLiteral = "nil"
+	HLTrue  HLLiteral = "#t"
+	HLFalse HLLiteral = "#f"
+)
+
+type HLList []HLValue
+
+type HLVector []HLValue
+
+type HLTablemap[HLValue]HLValue
+
+type HLFunction func(args ...HLType) (HLType, error)
